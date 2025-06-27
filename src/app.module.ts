80ca1f1541fd82913371c15928b7495f2bc07d3a
Tuấn Phạm <phamanhtuan290896@gmail.com>
2025-06-27 17:38:03 +0700

    update: touchit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import OrmConfig from './config/orm.config';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(OrmConfig.options), // Use the options from the OrmConfig
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/migrations/1751010960796-CreateUsersTable.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsersTable1751010960796 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'firstName',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'lastName',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users', true, true, true);
+  }
+}
